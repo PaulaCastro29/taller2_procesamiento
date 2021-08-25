d0ba680c59ae40bd445463cccdcea98e05c776c1
PaulaCastro29 <88325661+PaulaCastro29@users.noreply.github.com>
2021-08-24 19:34:08 -0500

    Add files via upload

---
+++ Taller2Punto1.py
@@ -0,0 +1,72 @@
+"""
+TALLER 2 Punto1- Paula Castro Aguilar
+Este archivo contiene la definición de la clase thetaFilter y sus respectivos métodos
+Corresponde al punto 1 del taller, donde el usuario ingresa los valores de teta y delta de teta y se obtiene el filtro de la imagen
+"""
+
+import cv2
+import sys
+import os
+import numpy as np
+
+class thetaFilter:
+    # Constructor que recibe la ruta de una imagen la carga y convierte la imagen en escala de grises
+    def __init__(self):
+        path = sys.argv[1]
+        image_name = sys.argv[2]
+        path_file = os.path.join(path, image_name)
+        image = cv2.imread(path_file)
+        self.image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        cv2.imshow("Original image",image)
+
+    # Método que recibe los valores de teta y delta de teta ingresados por el usuario
+    def set_theta(self,theta,theta_delta):
+        self.theta = theta
+        #print(self.theta)
+        self.theta_delta = theta_delta
+        #print(self.theta_delta)
+
+    # Método que aplica la trasnformada de Fourier a la imagen y aplica el filtro pasabandas teniendo en cuenta los valores de teta y delta ingresados
+    def filtering(self):
+        # Apply fft
+        image_gray_fft = np.fft.fft2(self.image_gray)
+        image_gray_fft_shift = np.fft.fftshift(image_gray_fft)
+        image_gray_fft_mag = np.absolute(image_gray_fft_shift)
+        # fft visualization
+        image_gray_fft_mag = np.absolute(image_gray_fft_shift)
+        image_fft_view = np.log(image_gray_fft_mag + 1)
+        image_fft_view = image_fft_view / np.max(image_fft_view)
+        # pre-computations
+        num_rows, num_cols = (self.image_gray.shape[0], self.image_gray.shape[1])
+        enum_rows = np.linspace(0, num_rows - 1, num_rows)
+        enum_cols = np.linspace(0, num_cols - 1, num_cols)
+        col_iter, row_iter = np.meshgrid(enum_cols, enum_rows)
+        half_size = num_rows / 2 - 1  # here we assume num_rows = num_columns
+
+        # Filtro
+        # band pass filter mask
+        band_pass_mask1 = np.zeros_like(self.image_gray)
+        idx_low = 180 * (np.arctan2(col_iter - half_size, row_iter - half_size)) / np.pi > (self.theta - self.theta_delta)
+        idx_high = 180 * (np.arctan2(col_iter - half_size, row_iter - half_size)) / np.pi < (self.theta + self.theta_delta)
+        idx_bp = np.bitwise_and(idx_low, idx_high)
+        band_pass_mask1[idx_bp] = 1
+
+        # filtering via FFT
+        mask = band_pass_mask1  # can also use high or band pass mask
+        fft_filtered = image_gray_fft_shift * mask
+        image_filtered1 = np.fft.ifft2(np.fft.fftshift(fft_filtered))
+        image_filtered1 = np.absolute(image_filtered1)
+        image_filtered1 /= np.max(image_filtered1)
+
+        cv2.imshow("Respuesta del filtro°", 255 * mask)
+        cv2.imshow("Imagen filtrada°", image_filtered1)
+        cv2.waitKey(0)
+
+if __name__ == '__main__':
+  Filter = thetaFilter()
+  theta = int(input("Ingrese valor de teta: "))
+  theta_delta = int(input("Ingrese valor de delta de teta: "))
+  Filter.set_theta(theta,theta_delta)
+  Filter.filtering()
+
+
---
+++ Taller2Punto2.py
@@ -0,0 +1,142 @@
+"""
+TALLER 2 Punto 2- Paula Castro Aguilar
+Este archivo contiene la definición de la clase thetaFilter y sus respectivos métodos
+Corresponde al punto 2 del taller, donde a partir de 4 filtros se promedia una imagen y se obtiene una nueva
+"""
+
+
+import cv2
+import sys
+import os
+import numpy as np
+
+
+class thetaFilter:
+    # Constructor que recibe la ruta de una imagen la carga y convierte la imagen en escala de grises
+    def __init__(self):
+        path = sys.argv[1]
+        image_name = sys.argv[2]
+        path_file = os.path.join(path, image_name)
+        image = cv2.imread(path_file)
+        self.image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        cv2.imshow("Original imagen",image)
+
+    # Método que recibe los valores de teta de cada filtro y delta de teta
+    def set_theta(self,theta1,theta2,theta3,theta4,theta_delta):
+        self.theta1 = theta1
+        self.theta2 = theta2
+        self.theta3 = theta3
+        self.theta4 = theta4
+        self.theta_delta = theta_delta
+
+    # Método que aplica la trasnformada de Fourier a la imagen y aplica los filtros y obtiene una imagen nueva con el promedio
+    def filtering(self):
+        # Apply fft
+        image_gray_fft = np.fft.fft2(self.image_gray)
+        image_gray_fft_shift = np.fft.fftshift(image_gray_fft)
+        image_gray_fft_mag = np.absolute(image_gray_fft_shift)
+        # fft visualization
+        image_gray_fft_mag = np.absolute(image_gray_fft_shift)
+        image_fft_view = np.log(image_gray_fft_mag + 1)
+        image_fft_view = image_fft_view / np.max(image_fft_view)
+        # pre-computations
+        num_rows, num_cols = (self.image_gray.shape[0], self.image_gray.shape[1])
+        enum_rows = np.linspace(0, num_rows - 1, num_rows)
+        enum_cols = np.linspace(0, num_cols - 1, num_cols)
+        col_iter, row_iter = np.meshgrid(enum_cols, enum_rows)
+        half_size = num_rows / 2 - 1  # here we assume num_rows = num_columns
+
+        # Filtro 0 grados
+        # band pass filter mask
+        band_pass_mask1 = np.zeros_like(self.image_gray)
+        idx_low = 180 * (np.arctan2(col_iter - half_size, row_iter - half_size)) / np.pi > (self.theta1 - self.theta_delta)
+        idx_high = 180 * (np.arctan2(col_iter - half_size, row_iter - half_size)) / np.pi < (self.theta1 + self.theta_delta)
+        idx_bp = np.bitwise_and(idx_low, idx_high)
+        band_pass_mask1[idx_bp] = 1
+
+        # filtering via FFT
+        mask = band_pass_mask1  # can also use high or band pass mask
+        fft_filtered = image_gray_fft_shift * mask
+        image_filtered1 = np.fft.ifft2(np.fft.fftshift(fft_filtered))
+        image_filtered1 = np.absolute(image_filtered1)
+        image_filtered1 /= np.max(image_filtered1)
+
+        cv2.imshow("Respuesta en frecuencia del filtro a 0 grados", 255 * mask)
+        cv2.imshow("Imagen filtrada a 0 grados", image_filtered1)
+        cv2.waitKey(0)
+
+        #filtro 45 grados
+        # band pass filter mask
+        band_pass_mask1 = np.zeros_like(self. image_gray)
+        idx_low = 180 * (np.arctan2(col_iter - half_size, row_iter - half_size)) / np.pi > (self.theta2 - self.theta_delta)
+        idx_high = 180 * (np.arctan2(col_iter - half_size, row_iter - half_size)) / np.pi < (self.theta2 + self.theta_delta)
+        idx_bp = np.bitwise_and(idx_low, idx_high)
+        band_pass_mask1[idx_bp] = 1
+
+        # filtering via FFT
+        mask = band_pass_mask1  # can also use high or band pass mask
+        fft_filtered = image_gray_fft_shift * mask
+        image_filtered2 = np.fft.ifft2(np.fft.fftshift(fft_filtered))
+        image_filtered2 = np.absolute(image_filtered2)
+        image_filtered2 /= np.max(image_filtered2)
+
+        cv2.imshow("Respuesta en frecuencia del filtro a 45 grados", 255 * mask)
+        cv2.imshow("Imagen filtrada a 45 grados", image_filtered2)
+        cv2.waitKey(0)
+
+        # Filtro 3 90°
+        # band pass filter mask
+        band_pass_mask1 = np.zeros_like(self.image_gray)
+        idx_low =180*(np.arctan2(col_iter - half_size, row_iter - half_size))/np.pi > (self.theta3 - self.theta_delta)
+        idx_high = 180 * (np.arctan2(col_iter - half_size, row_iter - half_size)) / np.pi < (self.theta3 + self.theta_delta)
+        idx_bp = np.bitwise_and(idx_low, idx_high)
+        band_pass_mask1[idx_bp] = 1
+
+        # filtering via FFT
+        mask = band_pass_mask1  # can also use high or band pass mask
+        fft_filtered = image_gray_fft_shift * mask
+        image_filtered3 = np.fft.ifft2(np.fft.fftshift(fft_filtered))
+        image_filtered3 = np.absolute(image_filtered3)
+        image_filtered3 /= np.max(image_filtered3)
+
+        cv2.imshow("Respuesta en frecuencia del filtro a 90 grados", 255 * mask)
+        cv2.imshow("Imagen filtrada a 90 grados", image_filtered3)
+        cv2.waitKey(0)
+
+    # Filtro 4 135°
+        # band pass filter mask
+        band_pass_mask1 = np.zeros_like(self.image_gray)
+        dt= 5 # it should less than freq_cut_off_high
+        t= 135 # it should less than 1
+
+        idx_low =180*(np.arctan2(col_iter - half_size, row_iter - half_size))/np.pi > (self.theta4 - self.theta_delta)
+        idx_high = 180 * (np.arctan2(col_iter - half_size, row_iter - half_size)) / np.pi < (self.theta4 + self.theta_delta)
+        idx_bp = np.bitwise_and(idx_low, idx_high)
+        band_pass_mask1[idx_bp] = 1
+
+        # filtering via FFT
+        mask = band_pass_mask1  # can also use high or band pass mask
+        fft_filtered = image_gray_fft_shift * mask
+        image_filtered4 = np.fft.ifft2(np.fft.fftshift(fft_filtered))
+        image_filtered4 = np.absolute(image_filtered4)
+        image_filtered4 /= np.max(image_filtered4)
+
+        cv2.imshow("Respuesta en frecuencia del filtro a 135 grados", 255 * mask)
+        cv2.imshow("Imagen filtrada a 135 grados", image_filtered4)
+        cv2.waitKey(0)
+
+        promedio = (image_filtered1+image_filtered2+image_filtered3+image_filtered4)/4
+        cv2.imshow("Promedio imagenes", promedio)
+        cv2.waitKey(0)
+        cv2.imwrite('promedio.png', promedio)
+
+if __name__ == '__main__':
+  Filter = thetaFilter()
+  theta1 = 0
+  theta2 = 45
+  theta3 = 90
+  theta4 = 135
+  theta_delta = 5
+  Filter.set_theta(theta1,theta2,theta3,theta4,theta_delta)
+  Filter.filtering()
+
